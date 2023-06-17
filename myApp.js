14c41c083919d1cfe3216a65fb7330e699c398c3
Felix Geelhaar <felix@felixgeelhaar.de>
2023-06-17 15:45:59 +0200

    feat: Introduce helmet

---
+++ myApp.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const helmet = require("helmet")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
+const api = require('./server.js');
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use(helmet())
+app.use('/_api', api);
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
+});
