9dec56d060843c7e37de8aa64c8c0066977db1c1
Felix Geelhaar <felix@felixgeelhaar.de>
2023-06-17 22:34:25 +0200

    feat: All all features for protected express

--- myApp.js
+++ myApp.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 const helmet = require("helmet")
-const serverless = require('serverless-http');
-
 
 
 
@@ -52,7 +50,22 @@ module.exports = serverless(app);
 const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
-app.use(helmet())
+
+const ninetyDaysInSeconds = 90*24*60*60;
+app.use(helmet({
+  xPoweredBy: false,
+  xFrameOptions: { action: "deny" },
+  xContentTypeOptions: false,
+  xDownloadOptions: false,
+  xXssProtection: false,
+  xDnsPrefetchControl: { allow: false },
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "'trusted-cdn.com'"]
+    }
+  },
+}))
 app.use('/.netlify/functions/api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
